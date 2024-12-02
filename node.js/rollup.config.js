3f23a504454b852f719c5043a6cd2bf1327aa6bc
thoughtsunificator <oss@thoughtsunificator.me>
2024-12-02 05:47:18 +0100

    fixup! Update

--- node.js/rollup.config.js
+++ node.js/rollup.config.js
@@ -1,44 +1,50 @@
-import postcss from "rollup-plugin-postcss"
-import postcssImport from "postcss-import"
-import postcssEasyImport  from "postcss-easy-import"
 import { terser } from "@wwa/rollup-plugin-terser"
 import copy from "rollup-plugin-copy-watch"
 import nodeResolve from "@rollup/plugin-node-resolve"
 import serve from "rollup-plugin-serve"
 import livereload from "rollup-plugin-livereload"
 import rollupWindowEnv from "@thoughtsunificator/rollup-plugin-window-env"
-import rollupWorkerFactory from "rollup-plugin-worker-factory"
 
 const isProduction = process.env.BUILD === "production"
 const isDevelopment = !isProduction
+const dist = isDevelopment ? "dist/app-dev" : "dist/app-prod"
 
-export default {
-	input: "src/main.js",
-	output: {
-		file: "dist/bundle.js",
-		format: "iife",
-		sourcemap: isDevelopment
+const plugins = [
+	isProduction && terser(),
+	copy({
+		watch: isDevelopment ? "public" : false,
+		targets: [{ src: "public/*", dest: dist }],
+		flatten: false
+	}),
+	nodeResolve(),
+	isDevelopment && livereload(dist),
+	isDevelopment && serve({
+		contentBase: dist,
+		host: process.env.HOST || "localhost",
+		port: process.env.PORT || 3000
+	}),
+]
+
+export default [
+	{
+		input: "src/worker/foo.js",
+		output: {
+			file: `${dist}/worker/foo.js`,
+			format: "esm",
+			sourcemap: isDevelopment
+		},
+		plugins
 	},
-	plugins: [
-		rollupWindowEnv({ envPath : ".env.json", confPath : "data/config.json" }),
-		postcss({
-			minimize: isProduction,
-			sourceMap: isDevelopment,
-			extract: true,
-			plugins: [postcssEasyImport(), postcssImport()]
-		}),
-		rollupWorkerFactory({ type: "universal" }),
-		isProduction && terser(),
-		copy({
-			watch: isDevelopment ? "public" : false,
-			targets: [{ src: "public/*", dest: "dist" }],
-			flatten: false
-		}),
-		nodeResolve(),
-		isDevelopment && livereload("dist"),
-		isDevelopment && serve({
-			contentBase: "dist",
-			port: process.env.PORT || 3000
-		}),
-	]
-}
+	{
+		input: "src/main.js",
+		output: {
+			file: `${dist}/bundle.js`,
+			format: "iife",
+			sourcemap: isDevelopment
+		},
+		plugins: [
+			rollupWindowEnv({ envPath : ".env.json", confPath : "data/config.json" }),
+			...plugins
+		]
+	}
+]
